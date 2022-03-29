bc282afbaa9607d06f237a472699880c74c29dcd
Louis Frayser <louis.frayser@gmail.com>
2022-03-28 22:40:11 -0700

    Moved manual-enty to a separate file

--- plan-c-data.rkt
+++ plan-c-data.rkt
@@ -10,15 +10,14 @@
 
 (define *input-plan*
   (with-input-from-file "db/manual-input.scm"
-    (lambda()
-    (let ((val (read)))
+    (lambda() (let ((val (read)))
       (if (and (pair? val) (eq? (car val) 'plan))
           (plan (cadr val) (caddr val) (cadddr val))
-          (error "Invalid user input: db/manual-input.scm!")
-          )))))
+          (error "Invalid user input: db/manual-input.scm!"))))))
     
   (define *plan-c*
     (plan "C" (symbol->string (plan-date *input-plan*))
           (->string (plan-groups *input-plan*))))
+
   (define (plan-categories a-plan)  (map car (plan-groups a-plan)))
   (plan-groups *plan-c*)
