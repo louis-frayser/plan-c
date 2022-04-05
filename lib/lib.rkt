a1aacdd738a958b57ad641be604f69c76486e458
Louis Frayser <louis.frayser@gmail.com>
2022-04-05 15:39:39 -0700

    Data validation of time string compares

--- lib/lib.rkt
+++ lib/lib.rkt
@@ -1,25 +1,24 @@
 #lang racket
 
-(provide ->string debug get-ymd-string strtime+)
+(provide ->string get-ymd-string strtime+)
 (require srfi/19)
 ;; ========================================================================
-(define (debug . values)
-  (newline)
-  (void (map (lambda(v)(printf "DEBUG: ~v\n" v)) values)))
 ;; -------------------------------------------------------------------------
 ;;; Add time-duration strings
 (define (strtime+ strt1 strt2)
-  (let*((2lists (map (lambda(s)(string-split s ":"))(list strt1 strt2)))
-        (2Nlists (map (lambda(ls)(map string->number ls)) 2lists))
-        (mins-ea (map (lambda(nlist)
-                        (+ (* 60 (car nlist)) (cadr nlist))) 2Nlists))
-        (tmins (apply + mins-ea)))
-    (call-with-values
-     (lambda()(quotient/remainder tmins 60))
-     (lambda(hrs mins)
-       (string-append
-        (~a hrs) ":" (~a mins #:align 'right 
-                         #:min-width 2 #:left-pad-string "0" ))))))
+  (with-handlers ((exn:fail? (lambda(exn)(eprintf "strtime+: ~v ~v ~v~n"
+                                                  strt1 strt2  exn))) )
+    (let*((2lists (map (lambda(s)(string-split s ":"))(list strt1 strt2)))
+          (2Nlists (map (lambda(ls)(map string->number ls)) 2lists))
+          (mins-ea (map (lambda(nlist)
+                          (+ (* 60 (car nlist)) (cadr nlist))) 2Nlists))
+          (tmins (apply + mins-ea)))
+      (call-with-values
+       (lambda()(quotient/remainder tmins 60))
+       (lambda(hrs mins)
+         (string-append
+          (~a hrs) ":" (~a mins #:align 'right 
+                           #:min-width 2 #:left-pad-string "0" )))))))
 ;; .......................................................................
 ;;; Get date string
 (define get-ymd-string (lambda() (date->string (current-date) "~1")))
