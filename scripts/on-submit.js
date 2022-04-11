193dbbdfd5601a0631bf5ea9e6e471b609daf970
Louis Frayser <louis.frayser@gmail.com>
2022-04-11 13:56:00 -0700

    Simplified html form logic

--- scripts/on-submit.js
+++ scripts/on-submit.js
@@ -3,12 +3,9 @@ import { Categories, ActionsByCatIx } from "./options-array.js";
 import { c,a,s, getDuration, getSelVal, getKey } from "./controls.js";
 
 export function validateSubmit(){
-    let cx = getSelVal(c),
-	ax = getSelVal(a),
-	cat = Categories[cx];
-
-    let act = ActionsByCatIx[cx][ax],
-	val = getDuration(),
+    let cat = getSelVal(c),
+	act = getSelVal(a),
+    	val = getDuration(),
 	ctext = ( "Category: " + cat + "\n" +
 		  "Activity: " + act + "\n" +
 		  "Duration: " + val );
