6a94e0aba2bcaa550286155b9be8f0f1b573d2aa
Louis Frayser <louis.frayser@gmail.com>
2022-04-17 03:42:20 -0700

    Further factored render vs forms-handling code

--- http-serv.rkt
+++ http-serv.rkt
@@ -1,22 +1,23 @@
 #lang racket
 
-(require web-server/servlet "lib/config.rkt" "lib/form-input.rkt")
+(require web-server/servlet
+         "lib/config.rkt" "lib/form-input.rkt" "lib/render.rkt")
 (provide/contract (start (request? . -> . response?)))
 
 (define (start req)
   (let* ((bindings (request-bindings req)))
     (when (exists-binding? 'change bindings)
-      (process-input-form bindings)
+      (process-input-form bindings render-page)
       (redirect/get)))
   (send/suspend/dispatch render-page))
 
 ;;; This starts the servelet with param "start respons/xepr" (above)
 (require web-server/servlet-env)
 (serve/servlet start
-	       #:launch-browser? #t
-	       #:quit? #f
-	       #:listen-ip #f
-	       #:port 8000
-	       #:extra-files-paths (list %orig-dir%
-					 (build-path %orig-dir% "htdocs"))
-	       #:servlet-path %servlet-path%)
+               #:launch-browser? #t
+               #:quit? #f
+               #:listen-ip #f
+               #:port 8000
+               #:extra-files-paths (list %orig-dir%
+                                         (build-path %orig-dir% "htdocs"))
+               #:servlet-path %servlet-path%)
--- lib/form-input.rkt
+++ lib/form-input.rkt
@@ -1,23 +1,19 @@
 #lang debug racket
 
-(provide handle-input-form process-input-form plan-report render-page (struct-out plan))
-(require web-server/servlet)
-(require "plan-c-data.rkt" "config.rkt" "render.rkt" debug "lib.rkt" "db-files.rkt")
-
-(define (render-page embed/url)
-  (response/xexpr #:preamble #"<!DOCTYPE html>\n"
-                   (plan-report embed/url handle-input-form)))
+(provide handle-input-form process-input-form)
+(require web-server/servlet debug
+         "plan-c-data.rkt" "config.rkt" "lib.rkt" "db-files.rkt")
 
 ;;; ==============================================================
 ;;;              INPUT FORM
-(define (handle-input-form req) (process-input-form (request-bindings req)))
-(define (process-input-form bindings)
+(define (handle-input-form req render-page) (process-input-form (request-bindings req)))
+(define (process-input-form bindings render-page)
   ;;; We are looking at a form that changes only one trie key (cat,act)
   ;;; The pair is of numerical indices representing strings in config
   ;;; config-schema-categories() and config-schema-subcategories()
   ;;; After decoding  the numbers into to strings, the struct is
   ;;; addressible for "update".
-  (void #RRR bindings); (debug-value "process-input-form with bindings: ~v"  bindings)
+  (void #RRR bindings); (debug-value "process-input-form.rkt: ~v" bindings)
   ;;; adding in the  time from the original activity
   (define (new-assocs orig-assocs new-assoc )
     (let*-values (( (key) (car new-assoc))
@@ -29,7 +25,7 @@
                        (car replace-assocs) (cons key "0:00"))))
       (let* ((base-dur (cdr matching-assoc))
              (xtra-dur (cdr new-assoc))
-             (tot-dur (strtime+ base-dur xtra-dur))
+             (tot-dur (string-time+ base-dur xtra-dur))
              (adj-new-assoc (cons (car new-assoc) tot-dur)))
         (cons adj-new-assoc keep-assocs))))
 
