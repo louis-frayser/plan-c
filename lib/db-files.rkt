e85c2fad7214d456d2f507a7edd336ad835c6f78
Louis Frayser <louis.frayser@gmail.com>
2022-05-08 02:18:01 -0700

    Refactor

--- lib/db-files.rkt
+++ lib/db-files.rkt
@@ -1,7 +1,8 @@
 #lang debug racket
 ; ====================================================================
 ;;; Disk Database....
-(provide (all-defined-out))
+(provide get-current-assoc-groups get-assocs get-assocs-by-datestr 
+         put-assoc-to-db write-file)
 
 (require srfi/19 "config.rkt" "lib.rkt" "plan-data.rkt")
 ;;; ------------------------------------------------------------------
@@ -13,42 +14,33 @@
   (with-output-to-file path
     (lambda() (displayln sexpr)) #:exists 'replace))
 
-(define file->assoc read-file)
 ;;; ------------------------------------------------------------------
-
-
 (define (get-current-assoc-groups) ; Get plan from permanent storage
+  (define (get-current-assocs)
+    (define assocs (get-assocs-by-datestr #:since (get-ymd-string)))
+    (if (pair? assocs)
+        (cadar assocs)
+        null))
   (assocs->groups (get-current-assocs)))
 
 ;;; .....................................................................
-
-;
 (define (put-assoc-to-db assoc) 
+  (define (put-assoc-to-db-for-date dstr assoc)
+    (define (get-assoc-pathname-for ymd-str)
+      (build-path
+       %db-base-dir% (date->string (current-date) "~1/assoc-~T.scm")))
+    (let* ((dest (get-assoc-pathname-for dstr))
+           (destdir (call-with-values
+                     (lambda()(split-path dest)) (lambda(dir _f _) dir))))
+      (unless (directory-exists? destdir)           
+        (make-directory destdir))
+      (with-output-to-file dest
+        ;; Don't use display or print!
+        (lambda()(writeln assoc)))))
   ;; put in db-basedir/yyyy-mm-dd
   (put-assoc-to-db-for-date (get-ymd-string) assoc))
   
-(define (put-assoc-to-db-for-date dstr assoc)
-  (define (get-assoc-pathname-for ymd-str)
-    (build-path
-     %db-base-dir% (date->string (current-date) "~1/assoc-~T.scm")))
-  (let* ((dest (get-assoc-pathname-for dstr))
-         (destdir (call-with-values
-                   (lambda()(split-path dest)) (lambda(dir _f _) dir))))
-    (unless (directory-exists? destdir)           
-      (make-directory destdir))
-    (with-output-to-file dest
-      ;; Don't use display or print!
-      (lambda()(writeln assoc)))))
 ;;; .....................................................................
-(define (get-assoc-paths-by-date #:since (beginning "2022-01-01"))
-  ;; ddate/directory
-  (define ddlist
-    (filter (curry string<=? beginning)
-            (map path->string (directory-list %db-base-dir%))))
-
-  ;; A list of date and associated files
-  (map (lambda(d) (list d (directory-list (build-path %db-base-dir% d))))
-       ddlist))
 
 (define (get-assocs #:since (beginning "2022-01-01"))
   (define (get-assoc-paths)
@@ -57,13 +49,23 @@
       (define date-part
         (let (( match (regexp-match #rx"[0-9]...-..-.." pathstr)))
           (and match (car match))))
-     (and (and date-part (string>=? date-part beginning))
-          (regexp-match #rx"2...-.*/assoc-.*.scm" pathstr)))
+      (and (and date-part (string>=? date-part beginning))
+           (regexp-match #rx"2...-.*/assoc-.*.scm" pathstr)))
     (find-files predicate %db-base-dir%
                 #:skip-filtered-directory? #f #:follow-links? #f))
+  (define file->assoc read-file)
   (map file->assoc (get-assoc-paths)))
 ;;; .....................................................................
+
 (define (get-assocs-by-datestr #:since (beginning "2022-01-01"))
+  (define (get-assoc-paths-by-date #:since (beginning "2022-01-01"))
+    ;; ddate/directory
+    (define ddlist
+      (filter (curry string<=? beginning)
+              (map path->string (directory-list %db-base-dir%))))
+    ;; A list of date and associated files
+    (map (lambda(d) (list d (directory-list (build-path %db-base-dir% d))))
+         ddlist))
   ;; Return association grouped by date. A starting date can be specified.
   (map
    (lambda(pr)
@@ -73,8 +75,3 @@
                 (second pr))))
    (get-assoc-paths-by-date #:since beginning)))
 
-(define (get-current-assocs)
-  (define assocs (get-assocs-by-datestr #:since (get-ymd-string)))
-  (if (pair? assocs)
-      (cadar assocs)
-      null))
