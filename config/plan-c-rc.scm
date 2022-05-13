474a010c72957047f0609e1c012a5a100e092367
Louis Frayser <louis.frayser@gmail.com>
2022-05-13 14:24:37 -0700

    Trying to default to production values

--- config/plan-c-rc.scm
+++ config/plan-c-rc.scm
@@ -2,7 +2,7 @@
 
 (provide (all-defined-out))
 
-(define %production% #f)
+(define %production% #t)
 (define %port% (if %production% 8008 8000))
 (define %version% (if %production% "0.0.1 pro" "0.0.1 dev"))
 
