b4f925dcb52c25316668d04bc744309b0ba3e72b
Louis Frayser <louis.frayser@gmail.com>
2023-06-08 09:02:12 -0700

    comments

--- config/plan-c-rc.scm
+++ config/plan-c-rc.scm
@@ -2,10 +2,11 @@
 
 (provide (all-defined-out))
 
-;; Default user for single-user-mode data
+;; Default user for single-user-mode data in db.rkt
 (define  %user% "frayser")
 
 ;; Database login credentials
 (define-values ( %pg_user%  %pg_db% %pg_pass%) (values "frayser" "frayser" ""))
 
+;; Defines where the 'target' indication is plotted
 (define %practice-target-mins% 150)
